1a91bac965bc173fe7b02d24d1599f6ce1a89ddb
Luís Pedro Coelho <lpc@cmu.edu>
2009-07-08 19:06:37 -0400

    Change version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ morph_src = map(lambda F: 'morph/'+F,['_morph.cpp'])
 morph = Extension('morph._morph', sources = morph_src, extra_compile_args=['-Wno-sign-compare'])
 
 setup (name = 'pymorph++',
-       version = '0.1',
+       version = '0.0.2',
        description = 'C++ implementation of morphological operations',
        packages=setuptools.find_packages(),
        ext_modules = [morph],
