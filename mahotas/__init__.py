31bee3f3dd05f1d7ace76f91021062a410ab163e
Luís Pedro Coelho <lpc@cmu.edu>
2010-01-05 11:25:35 -0500

    Merge the two directories into one.
    This is step two in the merging of these two projects.

--- mahotas/__init__.py
+++ mahotas/__init__.py
@@ -8,3 +8,6 @@ A set of functions for basic image processing and machine vision.
 from .histogram import fullhistogram
 from .thresholding import otsu
 from .pit import stretch
+import morph
+from .morph import *
+__doc__=morph.__doc__
--- morph/__init__.py
+++
@@ -1,3 +0,0 @@
-import morph
-from morph import *
-__doc__=morph.__doc__
