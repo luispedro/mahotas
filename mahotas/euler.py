1bff9a5c6b946b0b5d77c8f9b74e57a7889af0e3
Luis Pedro Coelho <lpc@cmu.edu>
2010-05-10 15:56:14 -0400

    Support 4 and 8 connectivity for euler()

--- mahotas/euler.py
+++ mahotas/euler.py
@@ -4,10 +4,16 @@
 
 import numpy as np
 from scipy import ndimage
-_euler_lookup = np.array([
+_euler_lookup4 = np.array([
             0,  1,  1,  0,
             1,  0,  2, -1,
             1,  2,  0, -1,
+            0, -1, -1,  0,
+            ])/4.
+_euler_lookup8 = np.array([
+            0,  1,  1,  0,
+            1,  0, -2, -1,
+            1, -2,  0, -1,
             0, -1, -1,  0,
             ])/4.
 _powers = np.array([
@@ -15,22 +21,42 @@ _powers = np.array([
     [4, 8]
     ])
 
-def euler(f):
+def euler(f, n=8):
     '''
-    euler_nr = euler(f)
+    euler_nr = euler(f, n=8)
 
     Compute the Euler number of image f
 
+    The Euler number is also known as the Euler characteristic given that many
+    other mathematical objects are also known as Euler numbers.
+
     Parameters
     ----------
-      f : A 2-D image
+      f : A 2-D binary image
+      n : Connectivity, one of (4,8). default: 8
     Returns
     -------
       euler_nr : Euler number
+
+    Reference
+    ---------
+    http://en.wikipedia.org/wiki/Euler_characteristic
+
+    Reference for Implementation
+    ----------------------------
+
+    *A Fast Algorithm for Computing the Euler Number of an Image and its VLSI
+    Implementation*, doi: 10.1109/ICVD.2000.812628
     '''
+    if n == 8:
+        lookup = _euler_lookup8
+    elif n == 4:
+        lookup = _euler_lookup4
+    else:
+        raise ValueError('mahotas.euler: Connectivity must be 4 or 8')
     if f.dtype is not np.bool:
         assert np.all( (f == 0) | (f == 1)), 'mahotas.euler: Non-binary image'
         f = (f != 0)
     value = ndimage.convolve(f.astype(_powers.dtype), _powers)
-    return _euler_lookup[value].sum()
+    return lookup[value].sum()
 
--- tests/test_euler.py
+++ tests/test_euler.py
@@ -1,4 +1,4 @@
-from mahotas.euler import euler, _euler_lookup
+from mahotas.euler import euler, _euler_lookup4, _euler_lookup8
 import numpy as np
 
 def test_lookup():
@@ -6,17 +6,20 @@ def test_lookup():
     Q2 =  [(~q) for q in Q1]
     Q3 = [np.array(q, np.bool) for q in [[0,1],[1,0]], [[1,0],[0,1]] ]
     
-    def _value(q):
+    def _value(q, lookup):
         q = q.ravel()
         value = np.dot(q, (1,2,4,8))
-        return _euler_lookup[value]
+        return lookup[value]
 
     for q in Q1:
-        assert _value(q) == .25
+        assert _value(q, _euler_lookup8) == .25
+        assert _value(q, _euler_lookup4) == .25
     for q in Q2:
-        assert _value(q) == -.25
+        assert _value(q, _euler_lookup8) == -.25
+        assert _value(q, _euler_lookup4) == -.25
     for q in Q3:
-        assert _value(q) == .5
+        assert _value(q, _euler_lookup8) == -.5
+        assert _value(q, _euler_lookup4) ==  .5
 
 def test_euler():
     f = np.zeros((16,16), np.bool)
