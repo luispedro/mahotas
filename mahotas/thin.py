fc9919eb3f998a8e8cbb7394368f3e852cbafeb5
Luis Pedro Coelho <lpc@cmu.edu>
2010-08-12 13:35:57 -0400

    Skeletonisation by thinning
    This is a simple implementation based on hitmiss()

---
+++ mahotas/thin.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2006-2010  Murphy Lab
+# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
+# Carnegie Mellon University
+#
+# Written by Luis Pedro Coelho <lpc@cmu.edu>, based on the 
+# Matlab implementation by the Murphy Lab
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation; either version 2 of the License,
+# or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+# For additional information visit http://murphylab.web.cmu.edu or
+# send email to murphy@cmu.edu
+
+from __future__ import division
+import numpy as np
+from .bbox import bbox
+from .morph import hitmiss
+
+__all__ = ['thin']
+
+_struct_elems = []
+_struct_elems.append([
+        [0,0,0],
+        [2,1,2],
+        [1,1,1]])
+_struct_elems.append([
+        [2,0,0],
+        [1,1,0],
+        [1,1,2]])
+_struct_elems.append([
+        [1,2,0],
+        [1,1,0],
+        [1,2,0]])
+_struct_elems.append([
+        [1,1,2],
+        [1,1,0],
+        [2,0,0]])
+_struct_elems.append([
+        [1,1,1],
+        [2,1,2],
+        [0,0,0]])
+_struct_elems.append([
+        [2,1,1],
+        [0,1,1],
+        [0,0,2]])
+_struct_elems.append([
+        [0,2,1],
+        [0,1,1],
+        [0,2,1]])
+_struct_elems.append([
+        [0,0,2],
+        [0,1,1],
+        [2,1,1]])
+
+_struct_elems = [np.array(elem, np.uint8) for elem in _struct_elems]
+
+def thin(binimg):
+    """
+    skel = thin(binimg)
+
+    Skeletonisation by thinning
+    """
+
+    res = np.zeros_like(binimg)
+    min0,max0,min1,max1 = bbox(binimg)
+
+    r,c = (max0-min0,max1-min1)
+
+    image_exp = np.zeros((r+2, c+2), np.uint8)
+    imagebuf = np.zeros((r+2,c+2), np.uint8)
+    image_exp[1:r+1, 1:c+1] = binimg[min0:max0,min1:max1]
+    while True:
+        changed = False
+        for elem in _struct_elems:
+            newimg = hitmiss(image_exp, elem, imagebuf)
+            image_exp -= newimg
+            changed |= newimg.any()
+        if not changed:
+            break
+    res[min0:max0,min1:max1] = image_exp[1:r+1, 1:c+1]
+    return res
+
---
+++ tests/test_thin.py
@@ -0,0 +1,10 @@
+import numpy as np
+import mahotas.thin
+import mahotas
+
+def test_thin():
+    A = np.zeros((100,100), bool)
+    A[20:40] = 1
+    W = mahotas.thin.thin(A)
+    assert mahotas.erode(W).sum() == 0
+
