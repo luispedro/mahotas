1afbf19f939e1067966917e9696d3026a60d57c8
Luis Pedro Coelho <lpc@cmu.edu>
2010-08-23 13:16:38 -0400

    Remove non-weave branch
    The weave branch will be removed, but it is the one to beat

--- mahotas/zernike.py
+++ mahotas/zernike.py
@@ -40,49 +40,38 @@ def _polar(r,theta):
 
 _factorialtable = np.array([1,1,2,6,24,120,720,5040,40320,362880,3628800,39916800,479001600])
 def Znl(n,l,X,Y,P):
-    v = 0.+0.j
-    try:
-        Nelems = len(X)
-        v = np.array([v]) # This is necessary for the C++ code to see and update v correctly
-        code='''
+    Nelems = len(X)
+    v = np.array([0 + 0.j]) # This is necessary for the C++ code to see and update v correctly
+    code='''
 #line 47 "zernike.py"
-        using std::pow;
-        using std::atan2;
-        using std::polar;
-        using std::conj;
-        using std::complex;
-        complex<double> Vnl = 0.0;
-        v(0)=0;
-        for (int i = 0; i != Nelems; ++i) {
-            double x=X(i);
-            double y=Y(i);
-            double p=P(i);
-            Vnl = 0.;
-            for(int m = 0; m <= (n-l)/2; m++) {
-                double f = (m & 1) ? -1 : 1;
-                Vnl += f * _factorialtable(int(n-m)) /
-                       ( _factorialtable(m) * _factorialtable((n - 2*m + l) / 2) * _factorialtable((n - 2*m - l) / 2) ) *
-                       ( pow( sqrt(x*x + y*y), (double)(n - 2*m)) ) *
-                       polar(1.0, l*atan2(y,x)) ;
-            }
-            v(0) += p * conj(Vnl);
+    using std::pow;
+    using std::atan2;
+    using std::polar;
+    using std::conj;
+    using std::complex;
+    complex<double> Vnl = 0.0;
+    v(0)=0;
+    for (int i = 0; i != Nelems; ++i) {
+        double x=X(i);
+        double y=Y(i);
+        double p=P(i);
+        Vnl = 0.;
+        for(int m = 0; m <= (n-l)/2; m++) {
+            double f = (m & 1) ? -1 : 1;
+            Vnl += f * _factorialtable(int(n-m)) /
+                   ( _factorialtable(m) * _factorialtable((n - 2*m + l) / 2) * _factorialtable((n - 2*m - l) / 2) ) *
+                   ( pow( sqrt(x*x + y*y), (double)(n - 2*m)) ) *
+                   polar(1.0, l*atan2(y,x)) ;
         }
-        '''
-        weave.inline(code,
-            ['_factorialtable','X','Y','P','v','n','l','Nelems'],
-            type_converters=converters.blitz,
-            compiler = 'gcc',
-            headers=['<complex>'])
-        v = v[0]
-    except e:
-        print 'Error:', e
-        for x,y,p in zip(X,Y,P):
-            Vnl = 0.
-            for m in xrange( (n-l)//2 + 1 ):
-                  Vnl += (-1.)**m * _factorialtable[n-m] /  \
-                ( _factorialtable[m] * _factorialtable[(n - 2*m + l) // 2] * _factorialtable[(n - 2*m - l) // 2] ) * \
-                ( np.sqrt(x*x + y*y)**(n - 2*m) * _polar(1.0, l*atan2(y,x)) )
-            v += p * math.conjugate(Vnl)
+        v(0) += p * conj(Vnl);
+    }
+    '''
+    weave.inline(code,
+        ['_factorialtable','X','Y','P','v','n','l','Nelems'],
+        type_converters=converters.blitz,
+        compiler = 'gcc',
+        headers=['<complex>'])
+    v = v[0]
     v *= (n+1)/np.pi
     return v
 
