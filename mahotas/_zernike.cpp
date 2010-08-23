d60091b926d38dbbfac8707c7e87b321d65cbe6c
Luis Pedro Coelho <lpc@cmu.edu>
2010-08-23 15:47:50 -0400

    Cache common computations

--- mahotas/_zernike.cpp
+++ mahotas/_zernike.cpp
@@ -1,5 +1,7 @@
 #include <complex>
 #include <cmath>
+#include <new>
+
 extern "C" {
     #include <Python.h>
     #include <numpy/ndarrayobject.h>
@@ -11,9 +13,15 @@ const char TypeErrorMsg[] =
     "Type not understood. "
     "This is caused by either a direct call to _zernike (which is dangerous: types are not checked!) or a bug in zernike.py.\n";
 
-double _factorialtable[] = { 
+double _factorialtable[] = {
         1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600
 };
+
+inline
+double fact(int n) {
+    if (unsigned(n) < sizeof(_factorialtable)/sizeof(double)) return _factorialtable[n];
+    return n * fact(n - 1);
+}
 
 PyObject* py_znl(PyObject* self, PyObject* args) {
     using std::pow;
@@ -42,21 +50,29 @@ PyObject* py_znl(PyObject* self, PyObject* args) {
     int Nelems = PyArray_SIZE(Da);
     complex<double> Vnl = 0.0;
     complex<double> v = 0.;
+    double * g_m = new(std::nothrow) double[ int( (n-l)/2 ) + 1];
+    if (!g_m) {
+        PyErr_NoMemory();
+        return NULL;
+    }
+    for(int m = 0; m <= (n-l)/2; m++) {
+        double f = (m & 1) ? -1 : 1;
+        g_m[m] = f * fact(n-m) /
+               ( fact(m) * fact((n - 2*m + l) / 2) * fact((n - 2*m - l) / 2) );
+    }
+
     for (int i = 0; i != Nelems; ++i) {
         double d=D[i];
         double a=A[i];
         double p=P[i];
         Vnl = 0.;
         for(int m = 0; m <= (n-l)/2; m++) {
-            double f = (m & 1) ? -1 : 1;
-            Vnl += f * _factorialtable[int(n-m)] /
-                   ( _factorialtable[m] * _factorialtable[int((n - 2*m + l) / 2)] * _factorialtable[int((n - 2*m - l) / 2)] ) *
-                   ( pow(d, (double)(n - 2*m)) ) *
-                   polar(1.0, l*a);
+            Vnl += g_m[m] * pow(d, (double)(n - 2*m)) * polar(1.0, l*a);
         }
         v += p * conj(Vnl);
     }
     v *= (n+1)/pi;
+    delete [] g_m;
     return PyComplex_FromDoubles(v.real(), v.imag());
 
 }
