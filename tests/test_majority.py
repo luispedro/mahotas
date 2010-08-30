9b7ca3aaf603024149715e5a9f3a02f5a722694b
Luis Pedro Coelho <lpc@cmu.edu>
2010-08-30 12:47:15 -0400

    Check that it's the dimensions that matter

--- tests/test_majority.py
+++ tests/test_majority.py
@@ -20,6 +20,9 @@ def test_majority():
     R = np.random.rand(64, 64) > .88
     assert np.all(mahotas.morph.majority_filter(R, 3) == slow_majority(R, 3))
 
+    R = np.random.rand(32, 64) > .88
+    assert np.all(mahotas.morph.majority_filter(R, 3) == slow_majority(R, 3))
+
     R = np.random.rand(64, 64) > .88
     assert np.all(mahotas.morph.majority_filter(R[:32,:], 3) == slow_majority(R, 3))
 
