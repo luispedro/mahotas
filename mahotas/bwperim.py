992c3d68712d29cd3b16dced719fa7f6d2e5e8f8
Luis Pedro Coelho <lpc@cmu.edu>
2010-09-02 16:47:54 -0400

    Add bwperim() function
    This was taken from octave and ported to Python.
    
    Implementation is simple and easily improved (in particular, there are
    several temporary arrays that could be easily removed).

---
+++ mahotas/bwperim.py
@@ -0,0 +1,79 @@
+# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
+#
+# This file was originally part of the octave-forge project
+# Ported to python by Luis Pedro Coelho <luis@luispedro.org> (February 2008)
+# Copyright (C) 2006       Soren Hauberg
+# Copyright (C) 2008-2010  Luis Pedro Coelho (Python port)
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details. 
+# 
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import division
+import numpy as np
+
+__all__ = ['bwperim']
+
+def bwperim(bw, n=4):
+    """
+    perim = bwperim(bw, n=4)
+
+    Find the perimeter of objects in binary images.
+
+    A pixel is part of an object perimeter if its value is one and there
+    is at least one zero-valued pixel in its neighborhood.
+
+    By default the neighborhood of a pixel is 4 nearest pixels, but
+    if `n` is set to 8 the 8 nearest pixels will be considered.
+
+    Parameters
+    ----------
+      bw : A black-and-white image
+      n : Connectivity. Must be 4 or 8 (default: 8)
+    Returns
+    -------
+      perim : A boolean image
+    """
+
+    if n not in (4,8):
+        raise ValueError('mahotas.bwperim: n must be 4 or 8')
+    rows,cols = bw.shape
+
+    # Translate image by one pixel in all directions
+    north = np.zeros((rows,cols))
+    south = np.zeros((rows,cols))
+    west = np.zeros((rows,cols))
+    east = np.zeros((rows,cols))
+
+    north[:-1,:] = bw[1:,:]
+    south[1:,:]  = bw[:-1,:]
+    west[:,:-1]  = bw[:,1:]
+    east[:,1:]   = bw[:,:-1]
+    idx = (north == bw) & \
+          (south == bw) & \
+          (west  == bw) & \
+          (east  == bw)
+    if n == 8:
+        north_east = np.zeros((rows, cols))
+        north_west = np.zeros((rows, cols))
+        south_east = np.zeros((rows, cols))
+        south_west = np.zeros((rows, cols))
+        north_east[:-1, 1:]   = bw[1:, :-1]
+        north_west[:-1, :-1] = bw[1:, 1:]
+        south_east[1:, 1:]     = bw[:-1, :-1]
+        south_west[1:, :-1]   = bw[:-1, 1:]
+        idx &= (north_east == bw) & \
+               (south_east == bw) & \
+               (south_west == bw) & \
+               (north_west == bw)
+    return ~idx * bw
+
---
+++ tests/test_bwperim.py
@@ -0,0 +1,33 @@
+from mahotas import bwperim
+import numpy as np
+
+def _neighbours(bwimg, y, x, n):
+    s0 = max(0, y-1)
+    e0 = min(y+2, bwimg.shape[0])
+    s1 = max(0, x-1)
+    e1 = min(x+2, bwimg.shape[1])
+    if n == 8:
+        return bwimg[s0:e0, s1:e1]
+    return np.concatenate([bwimg[s0:e0,x], bwimg[y,s1:e1]])
+
+def _slow_bwperim(bwimg, n=4):
+    r,c = bwimg.shape
+    res = np.zeros_like(bwimg)
+    for y in xrange(r):
+        for x in xrange(c):
+            res[y,x] = bwimg[y,x] and np.any(~_neighbours(bwimg,y,x,n))
+    return res
+
+def _compare_slow(img):
+    for n in (4,8):
+        assert np.all(_slow_bwperim(img, n) == bwperim(img, n))
+
+def test_bwperim():
+    img = np.zeros((8,8), np.bool)
+    img[3:7,3:7] = 1
+    _compare_slow(img)
+
+    assert img[3:7,3:7].all()
+    assert img[3:7,3:7].sum() == img.sum()
+    img[2,2:4] = 1
+    _compare_slow(img)
