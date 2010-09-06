b10df3ee2aef8bbea4bfdb1489bae435cb4a8cf5
Luis Pedro Coelho <lpc@cmu.edu>
2010-09-06 16:56:36 -0400

    Fix my name

--- mahotas/_histogram.cpp
+++ mahotas/_histogram.cpp
@@ -1,7 +1,7 @@
 // Copyright (C) 2008  Murphy Lab
 // Carnegie Mellon University
 // 
-// Written by Luís Pedro Coelho <lpc@cmu.edu>
+// Written by Luis Pedro Coelho <lpc@cmu.edu>
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published
