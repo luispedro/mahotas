38dc90028b72cc0712c357f461cb19e0b04a1a8f
Luis Pedro Coelho <lpc@cmu.edu>
2010-09-09 15:35:18 -0400

    100% coverage for majority_filter

--- tests/test_majority.py
+++ tests/test_majority.py
@@ -1,8 +1,10 @@
 import numpy as np
 import mahotas.morph
 from scipy import ndimage
+from nose.tools import raises
 
 def slow_majority(img, N):
+    img = (img > 0)
     r,c = img.shape
     output = np.zeros_like(img)
     for y in xrange(r-N):
@@ -15,20 +17,33 @@ def slow_majority(img, N):
                 output[y+dy//2,x+dx//2] = 1
     return output
 
+def compare_w_slow(R):
+    for N in (3,5,7):
+        assert np.all(mahotas.morph.majority_filter(R, N) == slow_majority(R, N))
+
 def test_majority():
-    def compare_w_slow(R):
-        assert np.all(mahotas.morph.majority_filter(R, 3) == slow_majority(R, 3))
 
     np.random.seed(22)
-    R = np.random.rand(64, 64) > .88
+    R = np.random.rand(64, 64) > .68
     yield compare_w_slow, R
 
-    R = np.random.rand(32, 64) > .88
+    R = np.random.rand(32, 64) > .68
     yield compare_w_slow, R
 
-    R = np.random.rand(64, 64) > .88
+    R = np.random.rand(64, 64) > .68
     yield compare_w_slow, R[:32,:]
 
-    R = np.random.rand(64, 64) > .88
+    R = np.random.rand(64, 64) > .68
     yield compare_w_slow, R[:23,:]
+
+
+@raises(ValueError)
+def test_N0():
+    mahotas.morph.majority_filter(np.zeros((20,20), np.bool_), 0)
+
+
+def test_not_bool():
+    np.random.seed(22)
+    R = np.random.rand(64, 64) > .68
+    yield compare_w_slow, R*24.
 
