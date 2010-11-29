7b81d4f1855ed892d1a6658c4df952ec39e969b0
Luis Pedro Coelho <lpc@cmu.edu>
2010-11-29 09:40:58 -0500

    TST: Test for convolution w/ 2x2 array
    This is currently causing a crash.

--- tests/test_convolve.py
+++ tests/test_convolve.py
@@ -8,3 +8,19 @@ def test_compare_w_ndimage():
     B = np.ones((3,3), A.dtype)
     for mode in mahotas._filters.modes:
         assert np.all(mahotas.convolve.convolve(A, B, mode=mode) == ndimage.convolve(A, B, mode=mode))
+
+def test_22():
+    A = np.arange(1024).reshape((32,32))
+    B = np.array([
+        [0,1],
+        [2,3],
+        ])
+    C = np.array([
+        [0,1,0],
+        [2,3,0],
+        [0,0,0],
+        ])
+    AB = mahotas.convolve.convolve(A,B)
+    AC = mahotas.convolve.convolve(A,C)
+    assert AB.shape == AC.shape
+    assert np.all(AB == AC)
