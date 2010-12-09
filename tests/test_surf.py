382f2f1166203e08255cb7dd72ec9e8b365315ee
Luis Pedro Coelho <lpc@cmu.edu>
2010-12-08 20:19:47 -0500

    TST: Test through Python interface

--- tests/test_surf.py
+++ tests/test_surf.py
@@ -1,8 +1,8 @@
 import numpy as np
-import mahotas._surf
+import mahotas.surf
 def test_integral():
     f = np.arange(8*16).reshape((8,16)) % 8
-    fi = mahotas._surf.integral(f.copy())
+    fi = mahotas.surf.integral(f.copy())
     fi[-1,-1] == f.sum()
     for y,x in np.indices(f.shape).reshape((2,-1)).T:
         assert fi[y,x] == f[:y+1,:x+1].sum()
