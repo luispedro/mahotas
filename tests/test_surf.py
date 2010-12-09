6f5a21fa3299d2d48ab750eb494fd8005b2b8ba2
Luis Pedro Coelho <lpc@cmu.edu>
2010-12-09 12:47:26 -0500

    TST: More testing

--- tests/test_surf.py
+++ tests/test_surf.py
@@ -3,6 +3,13 @@ import mahotas.surf
 def test_integral():
     f = np.arange(8*16).reshape((8,16)) % 8
     fi = mahotas.surf.integral(f.copy())
-    fi[-1,-1] == f.sum()
+    assert fi[-1,-1] == f.sum()
+    for y,x in np.indices(f.shape).reshape((2,-1)).T:
+        assert fi[y,x] == f[:y+1,:x+1].sum()
+
+def test_integral2():
+    f = np.arange(80*16).reshape((80,16)) % 7
+    fi = mahotas.surf.integral(f.copy())
+    assert fi[-1,-1] == f.sum()
     for y,x in np.indices(f.shape).reshape((2,-1)).T:
         assert fi[y,x] == f[:y+1,:x+1].sum()
