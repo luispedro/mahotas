abb18f98b7f5dca62a1a72f7090a4c616c822342
Luis Pedro Coelho <lpc@cmu.edu>
2010-12-20 01:55:17 -0500

    ENH: Add __all__ attribute
    Also, alphabetise the imports in __init__.py

--- mahotas/__init__.py
+++ mahotas/__init__.py
@@ -5,28 +5,64 @@ Mahotas
 
 A set of functions for basic image processing and machine vision.
 '''
-from .histogram import fullhistogram
-from .thresholding import otsu, rc
-from .stretch import stretch
-import morph
-from .morph import close_holes, get_structuring_elem, dilate, erode, cwatershed, majority_filter
-from .thin import thin
+from .bbox import bbox, croptobbox
 from .bwperim import bwperim
 from .center_of_mass import center_of_mass
-from .bbox import bbox, croptobbox
+from .convolve import convolve
+from .distance import distance
 from .edge import sobel
 from .euler import euler
+from .histogram import fullhistogram
 from .moments import moments
-from .distance import distance
+from .morph import close_holes, get_structuring_elem, dilate, erode, cwatershed, majority_filter
 from .resize import imresize
-from .convolve import convolve
+from .stretch import stretch
+from .thin import thin
+from .thresholding import otsu, rc
+
 from mahotas_version import __version__
 
-import segmentation
 import features
+import morph
+import segmentation
+
+__all__ = [
+    'bbox',
+    'bwperim',
+    'center_of_mass',
+    'close_holes',
+    'convolve',
+    'croptobbox',
+    'cwatershed',
+    'dilate',
+    'distance',
+    'erode',
+    'euler',
+    'fullhistogram',
+    'get_structuring_elem',
+    'imresize',
+    'majority_filter',
+    'moments',
+    'morph',
+    'otsu',
+    'rc',
+    'sobel',
+    'stretch',
+    'thin',
+
+    'features',
+    'morph',
+    'segmentation',
+
+    '__version__',
+    ]
 
 try:
     from .freeimage import imread, imsave
+    __all__ += [
+        'imread',
+        'imsave',
+        ]
 except OSError, e:
     def imread(*args, **kwargs):
         raise ImportError('mahotas.imread dependends on freeimage. Could not find it. Error was: %s' % e)
