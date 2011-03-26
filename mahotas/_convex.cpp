a07fa85e25a647709e15c3c915ddc2f23cc0fa56
Klaus-Michael Aye <kmichael.aye@gmail.com>
2011-03-26 20:33:17 +0100

    moved the Python.h include further down behind the C++ headers to get rid of configuration-dependent compile errors concerning C++ macros

--- mahotas/_convex.cpp
+++ mahotas/_convex.cpp
@@ -17,10 +17,10 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 // 02110-1301, USA.
 
-#include <Python.h>
 #include <algorithm>
 #include <vector>
 #include "utils.hpp"
+#include <Python.h>
 
 namespace {
 struct Point {
