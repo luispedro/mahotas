12ad8446b21a3549a8bb2de591eac619e4db9a7f
Luis Pedro Coelho <luis@luispedro.org>
2011-06-23 16:35:45 -0400

    TST Test _get_output
    This is one additional reason to factor out this function: easier to
    test it and other functions (where one will not need to test error code
    paths and still have everything be correctly covered).

---
+++ mahotas/tests/test_get_output.py
@@ -0,0 +1,26 @@
+import numpy as np
+from mahotas.internal import _get_output
+from nose.tools import raises
+
+def test_get_output():
+    f = np.arange(256).reshape((32,8))
+    output = _get_output(f, None, 'test')
+    assert output.dtype == f.dtype
+    assert output.shape == f.shape
+    out2 = _get_output(f, output, 'test')
+    assert out2 is output
+
+@raises(ValueError)
+def test_get_output_bad_shape():
+    f = np.arange(256).reshape((32,8))
+    output = np.zeros((16,16), f.dtype)
+    _get_output(f, output, 'testing')
+    
+@raises(ValueError)
+def test_get_output_non_contiguous():
+    f = np.arange(256).reshape((32,8))
+    output = np.zeros((32,16), f.dtype)
+    output = output[:,::2]
+    assert output.shape == f.shape
+    _get_output(f, output, 'testing')
+    
