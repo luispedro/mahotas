ea871eb17034604be10ff1b5e6733fe123ea445a
Luis Pedro Coelho <luis@luispedro.org>
2012-02-13 17:12:15 +0000

    ENH More robust freeimaging
    If a certain function does not exist, fall back on an error function.

--- mahotas/freeimage.py
+++ mahotas/freeimage.py
@@ -99,10 +99,15 @@ class _ctypes_wrapper(object):
 def _register_api(lib, api):
     nlib = _ctypes_wrapper()
     for f, (restype, argtypes) in api.iteritems():
-        func = getattr(lib, f)
-        func.restype = restype
-        func.argtypes = argtypes
-        setattr(nlib, f, func)
+        try:
+            func = getattr(lib, f)
+            func.restype = restype
+            func.argtypes = argtypes
+            setattr(nlib, f, func)
+        except:
+            def error_raise(*args):
+                raise RuntimeError('mahotas.freeimage: Function `%s` not found in your version of FreeImage. It might be an older version' % f)
+            setattr(nlib, f, error_raise)
     return nlib
 
 libname = ctypes.util.find_library('freeimage')
