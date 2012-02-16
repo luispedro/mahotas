3331e7aa18537f292f717d10c2a7d2ef268a4cc6
Luis Pedro Coelho <luis@luispedro.org>
2012-02-16 16:29:18 +0000

    TST Stricter test for dilate/erode

--- mahotas/tests/test_dilate_erode.py
+++ mahotas/tests/test_dilate_erode.py
@@ -9,10 +9,11 @@ def test_dilate_erode():
         [0, 1, 0]], bool)
     A[30,30] = 1
     A = (A!=0)
-    orig = A.copy()
+    origs = []
     for i in xrange(12):
+        origs.append(A.copy())
         A = mahotas.dilate(A, Bc)
     for i in xrange(12):
         A = mahotas.erode(A, Bc)
-    assert np.all(A == orig)
+        assert np.all(A == origs[-i-1])
 
