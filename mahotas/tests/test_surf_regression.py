9d38c9c8956e7a330ba36cae639da5566ea019e7
Luis Pedro Coelho <luis@luispedro.org>
2012-03-08 12:41:28 +0000

    TST Fix weird PNG file
    Originally, the file `determinant_zero.png` contained a PNG which
    FreeImage was opening incorrectly (it returned the palette instead of
    mapping through to the colours).
    
    If correctly mapped, the image was actually full of zeros (i.e., all
    colours in the palette were (0,0,0)!)
    
    Therefore, there are actually two tests here:
        with a zero image
        with the values of the palette in determinant_zero.png
    
    The file `determinant_zero.png` now contains what was originally the
    palette values.

--- mahotas/tests/test_surf_regression.py
+++ mahotas/tests/test_surf_regression.py
@@ -3,11 +3,30 @@ import numpy as np
 import mahotas.surf
 from os import path
 
+# Originally, the file `determinant_zero.png` contained a PNG which FreeImage
+# was opening incorrectly (it returned the palette instead of mapping through
+# to the colours).
+#
+# If correctly mapped, the image was actually full of zeros (i.e., all colours
+# in the palette were (0,0,0)!)
+#
+# Therefore, there are actually two tests here:
+#     with a zero image
+#     with the values of the palette in determinant_zero.png
+#
+# The file `determinant_zero.png` now contains what was originally the palette
+# values.
+
 def test_determinant_zero():
     img = mahotas.imread(path.join(
         path.abspath(path.dirname(__file__)),
                     'data',
                     'determinant_zero.png'))
+    points = mahotas.surf.surf(img, threshold=.0)
+    assert type(points) == np.ndarray
+
+def test_determinant_zero2():
+    img = np.zeros((128,28), np.uint8)
     points = mahotas.surf.surf(img, threshold=.0)
     assert type(points) == np.ndarray
 
