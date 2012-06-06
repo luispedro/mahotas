8faa80a264d44f3ea81057f15a0d304c3aaaebd1
Luis Pedro Coelho <luis@luispedro.org>
2012-06-06 13:02:19 +0100

    ENH Change morph to MIT license

--- mahotas/_morph.cpp
+++ mahotas/_morph.cpp
@@ -1,3 +1,8 @@
+// Copyright (C) 2008-2012, Luis Pedro Coelho <luis@luispedro.org>
+// vim: set ts=4 sts=4 sw=4 expandtab smartindent:
+// 
+// License: MIT
+
 #include <algorithm>
 #include <queue>
 #include <vector>
