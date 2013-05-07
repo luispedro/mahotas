462bde56e524997f54c680ac1676f20e9e17deb0
Luis Pedro Coelho <luis@luispedro.org>
2013-05-07 14:43:01 +0200

    TST Skip testing of IO dependent functionality

--- mahotas/tests/test_io.py
+++ mahotas/tests/test_io.py
@@ -9,6 +9,19 @@ filename = path.join(
             'data',
             'rgba.png')
 
+
+def skip_on(etype):
+    def skip_on2(test):
+        try:
+            yield test
+        except e:
+            if isinstance(e, etype):
+                from nose import SkipTest
+                raise SkipTest
+            raise
+    return skip_on2
+
+
 @raises(ImportError)
 def test_error_imread():
     error_imread(filename)
@@ -17,7 +30,7 @@ def test_error_imread():
 def test_error_imsave():
     error_imsave('/tmp/test_mahotas.png', np.arange(16, dtype=np.uint8).reshape((4,4)))
 
-
+@skip_on(IOError)
 def test_as_grey():
     filename = path.join(
             path.dirname(__file__),
