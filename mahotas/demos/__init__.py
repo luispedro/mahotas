169e61e61594251d1d3d80ccfbb22a2340a4d56e
Luis Pedro Coelho <luis@luispedro.org>
2013-06-15 13:50:54 +0200

    ENH Add mahotas.demos.nuclear_image() function

--- mahotas/demos/__init__.py
+++ mahotas/demos/__init__.py
@@ -4,3 +4,8 @@ def image_path(name):
     return path.join(path.abspath(path.dirname(__file__)),
                 'data',
                 name)
+
+
+def nuclear_image():
+    import mahotas as mh
+    return mh.imread(image_path('nuclear.png'))
