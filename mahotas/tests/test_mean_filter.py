827632bfd3944d2437b393383088b1f3efa3410a
Luis Pedro Coelho <luis@luispedro.org>
2014-04-07 18:47:50 +0200

    PY3 Use range() for tests
    Muscle memory just spells xrange() all the time

--- mahotas/tests/test_mean_filter.py
+++ mahotas/tests/test_mean_filter.py
@@ -5,7 +5,7 @@ def test_smoke():
     f = np.random.random((512,1024))
     se = np.ones((2,2))
     ff = mean_filter(f, se)
-    for _ in xrange(128):
+    for _ in range(128):
         a = np.random.randint(1, f.shape[0] - 1)
         b = np.random.randint(1, f.shape[1] - 1)
         assert np.allclose(ff[a,b], np.mean(f[a-1:a+1, b-1:b+1]))
