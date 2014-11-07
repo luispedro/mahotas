BUG Fix Zernike moments computation
It was possible to perform division by zero and thus get NaNs in the
output.

Reported by Sergey Demurin.

mahotas/features/zernike.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2006-2012, Luis Pedro Coelho <luis@luispedro.org>
+# Copyright (C) 2006-2014, Luis Pedro Coelho <luis@luispedro.org>
 # vim: set ts=4 sts=4 sw=4 expandtab smartindent:
 #
 # License: MIT (see COPYING file)
@@ -74,6 +74,7 @@ def zernike_moments(im, radius, degree=8, cm=None):
     Dn = Xn**2
     Dn += Yn**2
     np.sqrt(Dn, Dn)
+    np.maximum(Dn, 1e-9, out=Dn)
     k = (Dn <= 1.)
     k &= (P > 0)
 

mahotas/tests/test_zernike.py
@@ -1,7 +1,8 @@
 import numpy as np
 from mahotas.features import zernike_moments
 from mahotas.center_of_mass import center_of_mass
-from math import atan2
+import mahotas as mh
+from math import atan2, ceil
 from numpy import cos, sin, conjugate, pi, sqrt
 
 def _slow_znl(Y,X,P,n,l):
@@ -65,3 +66,27 @@ def test_zernike_cm():
     delta = np.array(slow) - fast
     assert np.abs(delta).max() < 0.001
 
+def test_not_nan():
+    im = np.array([
+           [  0,   0,  38, 168,  78,   0,   0,   0, 126, 168, 126,   0,   0],
+           [  0,   0, 104, 168,  78,   0,   0,  78, 168, 169, 169,  78,   0],
+           [  0,  38, 169, 169,  78,   0,   0, 142, 169, 169, 169, 162,   0],
+           [ 26, 156, 169, 168,  78,   0,   0, 169, 142,   0, 126, 169,  38],
+           [142, 169, 156, 166,  78,   0,  38, 169, 104,   0,  78, 169,  64],
+           [126, 142, 104, 168,  78,   0,  64, 168,  78,   0,  64, 168,  78],
+           [104,   0,  78, 169,  78,   0,  78, 168,  78,   0,  52, 169,  78],
+           [  0,   0,  78, 169,  78,   0,  78, 166,  78,   0,  52, 169,  78],
+           [  0,   0,  78, 166,  78,   0,  78, 166,  78,   0,  52, 169,  78],
+           [  0,   0,  78, 169,  78,   0,  78, 166,  78,   0,  52, 169,  78],
+           [  0,   0,  78, 169,  78,   0,  78, 166,  78,   0,  52, 169,  78],
+           [  0,   0,  78, 169,  78,   0,  78, 166,  78,   0,  64, 169,  78],
+           [  0,   0,  78, 166,  78,   0,  52, 166, 104,   0,  78, 169,  64],
+           [  0,   0,  78, 169,  78,   0,   0, 169, 142,   0, 126, 169,  38],
+           [  0,   0,  78, 169,  78,   0,   0, 142, 169, 169, 169, 156,   0],
+           [  0,   0,  78, 169,  78,   0,   0,  78, 168, 169, 169,  78,   0],
+           [  0,   0,  78, 166,  78,   0,   0,   0, 104, 169, 126,   0,   0]], dtype=np.uint8)
+
+    yy, xx = np.nonzero(im)
+    (cx, cy), r = (6., 8.), 9.212599754333496 # cv2.minEnclosingCircle(points)
+    zms = mh.features.zernike_moments(im, r, 8, cm=(cx, cy))
+    assert not np.any(np.isnan(zms))