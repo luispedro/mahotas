1840b629fab325e7fb700f90cf2b662ff35e4205
Luis Pedro Coelho <luis@luispedro.org>
2017-06-14 02:26:21 +0200

    TST Better travis cache handling
    This should cache more information and better.

--- .travis/travis_before_install.sh
+++ .travis/travis_before_install.sh
@@ -10,7 +10,7 @@ else
     wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
     chmod +x miniconda.sh
     ./miniconda.sh -b -p $HOME/miniconda
+    conda update --yes --quiet conda
 fi
 export PATH=$HOME/miniconda/bin:$PATH
-conda update --yes --quiet conda
 
