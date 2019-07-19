fc80bc8d663f3999394cdcc608a237405cc77442
Luis Pedro Coelho <luis@luispedro.org>
2019-07-20 01:13:06 +0200

    RFCT Update demo
    - use mh.demos.load
    - use "import mahotas as mh"
    - do not use the name "c" for two objects

--- mahotas/demos/nuclear_distance_watershed.py
+++ mahotas/demos/nuclear_distance_watershed.py
@@ -1,37 +1,29 @@
-import mahotas
+import mahotas as mh
 from os import path
 import numpy as np
 from matplotlib import pyplot as plt
 
-try:
-    nuclear_path = path.join(
-                    path.dirname(path.abspath(__file__)),
-                    'data',
-                    'nuclear.png')
-except NameError:
-    nuclear_path = path.join('data', 'nuclear.png')
-
-nuclear = mahotas.imread(nuclear_path)
+nuclear = mh.demos.load('nuclear')
 nuclear = nuclear[:,:,0]
-nuclear = mahotas.gaussian_filter(nuclear, 1.)
+nuclear = mh.gaussian_filter(nuclear, 1.)
 threshed  = (nuclear > nuclear.mean())
-distances = mahotas.stretch(mahotas.distance(threshed))
+distances = mh.stretch(mh.distance(threshed))
 Bc = np.ones((9,9))
 
-maxima = mahotas.morph.regmax(distances, Bc=Bc)
-spots,n_spots = mahotas.label(maxima, Bc=Bc)
+maxima = mh.morph.regmax(distances, Bc=Bc)
+spots,n_spots = mh.label(maxima, Bc=Bc)
 surface = (distances.max() - distances)
-areas = mahotas.cwatershed(surface, spots)
+areas = mh.cwatershed(surface, spots)
 areas *= threshed
 
 
 
 import random
-from matplotlib import colors as c
+from matplotlib import colors
 from matplotlib import cm
-colors = [cm.jet(c) for c in range(0, 256, 4)]
-random.shuffle(colors)
-colors[0] = (0.,0.,0.,1.)
-rmap = c.ListedColormap(colors)
+cols = [cm.jet(c) for c in range(0, 256, 4)]
+random.shuffle(cols)
+cols[0] = (0.,0.,0.,1.)
+rmap = colors.ListedColormap(cols)
 plt.imshow(areas, cmap=rmap)
 plt.show()
