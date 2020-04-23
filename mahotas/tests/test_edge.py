2ec77a876570253082a33d46b3353b7f0d407c0f
Sujoy Kumar Goswami <sujoykumargoswami@gmail.com>
2020-04-23 20:48:32 +0530

    Update test_edge.py
    added test cases for 'sujoy'

--- mahotas/tests/test_edge.py
+++ mahotas/tests/test_edge.py
@@ -1,4 +1,5 @@
 from mahotas.edge import sobel
+from mahotas.edge import sujoy
 from nose.tools import raises
 import mahotas as mh
 import numpy as np
@@ -37,7 +38,6 @@ def test_zero_images():
     assert np.isnan(sobel(np.zeros((16,16)))).sum() == 0
     assert sobel(np.zeros((16,16)), just_filter=True).sum() == 0
 
-
 def test_sobel_pure():
     f = np.random.random((64, 128))
     f2 = f.copy()
@@ -45,10 +45,52 @@ def test_sobel_pure():
     assert np.all(f == f2)
 
 
+ def test_sujoy_shape():
+    A = np.arange(100*100)
+    A = (A % 15)
+    A = A.reshape((100,100))
+    assert sujoy(A).shape == A.shape
+    assert sujoy(A, just_filter=True).shape == A.shape
+    
+def test_sujoy_zeros():
+    A = np.zeros((15,100))
+    assert sujoy(A).shape == A.shape
+    assert sujoy(A).sum() == 0
+    
+def test_sujoy():
+    I = np.array([
+            [0,0,0,0,0,0],
+            [0,0,0,1,0,0],
+            [0,0,0,1,0,0],
+            [0,0,0,1,0,0],
+            [0,0,0,1,0,0],
+            [0,0,0,0,0,0]])
+    E = sujoy(I)
+    r,c = I.shape
+    for y,x in zip(*np.where(E)):
+        N = [I[y,x]]
+        if y > 0: N.append(I[y-1,x])
+        if x > 0: N.append(I[y,x-1])
+        if y < (r-1): N.append(I[y+1,x])
+        if x < (c-1): N.append(I[y,x+1])
+        assert len(set(N)) > 1
+        
+def test_zero_images1():
+    assert np.isnan(sujoy(np.zeros((16,16)))).sum() == 0
+    assert sujoy(np.zeros((16,16)), just_filter=True).sum() == 0
+
+def test_sujoy_pure():
+    f = np.random.random((64, 128))
+    f2 = f.copy()
+    _ = mh.sujoy(f)
+    assert np.all(f == f2)
+    
+    
 @raises(ValueError)
 def test_3d_error():
     f = np.zeros((32,16,3))
     sobel(f)
+    sujoy(f)
 
 
 def test_dog():
