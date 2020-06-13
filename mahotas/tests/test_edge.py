d9283fef876a7e3d6c8764e3c48d98590828cb93
Sujoy Kumar Goswami <sujoykumargoswami@gmail.com>
2020-06-13 21:55:55 +0530

    all indentation are now spaces

--- mahotas/tests/test_edge.py
+++ mahotas/tests/test_edge.py
@@ -52,49 +52,49 @@ def test_3d_error():
     sobel(f)
 
 def test_sujoy_shape():
-	A = np.arange(100*100)
-	A = (A % 15)
-	A = A.reshape((100,100))
-	assert sujoy(A).shape == A.shape
-	assert sujoy(A, just_filter=True).shape == A.shape
+    A = np.arange(100*100)
+    A = (A % 15)
+    A = A.reshape((100,100))
+    assert sujoy(A).shape == A.shape
+    assert sujoy(A, just_filter=True).shape == A.shape
 
 def test_sujoy_zeros():
-	A = np.zeros((15,100))
-	assert sujoy(A).shape == A.shape
-	assert sujoy(A).sum() == 0
+    A = np.zeros((15,100))
+    assert sujoy(A).shape == A.shape
+    assert sujoy(A).sum() == 0
 
 def test_sujoy():
-	I = np.array([
-		[0,0,0,0,0,0],
-		[0,0,0,1,0,0],
-		[0,0,0,1,0,0],
-		[0,0,0,1,0,0],
-		[0,0,0,1,0,0],
-		[0,0,0,0,0,0]])
-	E = sujoy(I)
-	r,c = I.shape
-	for y,x in zip(*np.where(E)):
-		N = [I[y,x]]
-		if y > 0: N.append(I[y-1,x])
-		if x > 0: N.append(I[y,x-1])
-		if y < (r-1): N.append(I[y+1,x])
-		if x < (c-1): N.append(I[y,x+1])
-		assert len(set(N)) > 1
+    I = np.array([
+        [0,0,0,0,0,0],
+        [0,0,0,1,0,0],
+        [0,0,0,1,0,0],
+        [0,0,0,1,0,0],
+        [0,0,0,1,0,0],
+        [0,0,0,0,0,0]])
+    E = sujoy(I)
+    r,c = I.shape
+    for y,x in zip(*np.where(E)):
+        N = [I[y,x]]
+        if y > 0: N.append(I[y-1,x])
+        if x > 0: N.append(I[y,x-1])
+        if y < (r-1): N.append(I[y+1,x])
+        if x < (c-1): N.append(I[y,x+1])
+        assert len(set(N)) > 1
 
 def test_zero_images1():
-	assert np.isnan(sujoy(np.zeros((16,16)))).sum() == 0
-	assert sujoy(np.zeros((16,16)), just_filter=True).sum() == 0
+    assert np.isnan(sujoy(np.zeros((16,16)))).sum() == 0
+    assert sujoy(np.zeros((16,16)), just_filter=True).sum() == 0
 
 def test_sujoy_pure():
-	f = np.random.random((64, 128))
-	f2 = f.copy()
-	_ = mh.sujoy(f)
-	assert np.all(f == f2)
+    f = np.random.random((64, 128))
+    f2 = f.copy()
+    _ = mh.sujoy(f)
+    assert np.all(f == f2)
 
 @raises(ValueError)
 def test_3d_error1():
-	f = np.zeros((32,16,3))
-	sujoy(f)
+    f = np.zeros((32,16,3))
+    sujoy(f)
 
 def test_dog():
     im = mh.demos.load('lena')
