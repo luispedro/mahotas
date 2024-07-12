982242a4169e36a24f13b8c477e92bb49cea0ff4
Luis Pedro Coelho <luis@luispedro.org>
2024-07-12 11:07:43 -0400

    BUG Add missing NPY_ULONG to dispatch list

--- mahotas/numpypp/dispatch.hpp
+++ mahotas/numpypp/dispatch.hpp
@@ -6,13 +6,19 @@ typedef unsigned char uchar;
 typedef unsigned short ushort;
 #define HANDLE_INTEGER_TYPES() \
     case NPY_BOOL: HANDLE(bool); break; \
+    \
     case NPY_UBYTE: HANDLE(unsigned char); break; \
     case NPY_BYTE: HANDLE(char); break; \
+    \
     case NPY_SHORT: HANDLE(short); break; \
     case NPY_USHORT: HANDLE(unsigned short); break; \
+    \
     case NPY_INT: HANDLE(int); break; \
     case NPY_UINT: HANDLE(unsigned int); break; \
-    case NPY_LONG: HANDLE(npy_long); break; \
+    \
+    case NPY_LONG:  HANDLE(npy_long); break; \
+    case NPY_ULONG: HANDLE(npy_ulong); break; \
+    \
     case NPY_LONGLONG: HANDLE(npy_longlong); break; \
     case NPY_ULONGLONG: HANDLE(npy_ulonglong); break;
 
